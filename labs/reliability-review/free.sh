816b9d7dd1c03e408931fe59af17de5fc10b96c2
lahmars <31050003+lahmars@users.noreply.github.com>
2026-06-07 17:41:39 +0200

    Update free.sh by removing copyright and changing HOST
    Removed copyright notice and updated HOST variable.

--- labs/reliability-review/free.sh
+++ labs/reliability-review/free.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-#
-# Copyright (c) 2023 Red Hat Training <training@redhat.com>
-#
-# All rights reserved.
-# No warranty, explicit or implied, provided.
 
-HOST=master01.ocp4.example.com
+HOST={{ CHANGE ME }} #put cluster node
 PORT=$(oc get service longload -o jsonpath='{.spec.ports[0].nodePort}')
 
 curl http://${HOST}:${PORT}/unleak
