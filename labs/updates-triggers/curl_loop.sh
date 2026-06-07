e37080df5d08facae63f5b1f8ca500d44a99080f
lahmars <31050003+lahmars@users.noreply.github.com>
2026-06-07 18:47:31 +0200

    Remove copyright comments from curl_loop.sh
    Removed copyright notice from curl_loop.sh

--- labs/updates-triggers/curl_loop.sh
+++ labs/updates-triggers/curl_loop.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#
-# Copyright (c) 2023 Red Hat Training <training@redhat.com>
-#
-# All rights reserved.
-# No warranty, explicit or implied, provided.
 
 HOST=$(oc get route version -o jsonpath='{.spec.host}')
 
